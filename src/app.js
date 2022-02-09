14307d260ca0a2ca92254150a056b5c6f6636812
TamasParkany <thomasovmail@gmail.com>
2022-02-10 00:02:28 +0100

    CSS: pre-npm

---
+++ src/app.js
@@ -0,0 +1,19 @@
+history.scrollRestoration = "manual";
+
+const formButtons = document.querySelectorAll(".form");
+const contactForm = document.getElementById("contact-form");
+
+formButtons.forEach((button) => button.addEventListener("click", showForm));
+contactForm.addEventListener("click", hideForm);
+
+function showForm() {
+  contactForm.style.display = "flex";
+}
+
+function hideForm(e) {
+  if (e.target.id === "contact-form") {
+    contactForm.style.display = "none";
+  } else {
+    return;
+  }
+}
