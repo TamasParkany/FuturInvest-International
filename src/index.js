3250e803d2f0dd670e6d26dd3a1e4ed541088440
TamasParkany <thomasovmail@gmail.com>
2022-02-10 20:16:47 +0100

    hamburger menu

--- src/index.js
+++ src/index.js
@@ -19,15 +19,34 @@ function hideForm(e) {
 }
 
 //hide navbar on scroll
+
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    header.style.top = "0";
-    headerBg.style.top = "0";
-  } else {
-    header.style.top = "-150px";
-    headerBg.style.top = "-150px";
+  if (window.screen.width > 1180) {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      header.style.top = "0";
+      headerBg.style.top = "0";
+    } else {
+      header.style.top = "-150px";
+      headerBg.style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
 };
+
+// mobile menu
+document
+  .getElementsByClassName("hamburger")[0]
+  .addEventListener("click", toggleMMenu);
+document
+  .querySelectorAll(".mobile-nav div a")
+  .forEach((a) => a.addEventListener("click", toggleMMenu));
+
+function toggleMMenu() {
+  let circle = document.getElementsByClassName("circle")[0];
+  let menu = document.getElementsByClassName("mobile-nav")[0];
+
+  circle.classList.toggle("circle-active");
+  menu.classList.toggle("mobile-nav-active");
+}
