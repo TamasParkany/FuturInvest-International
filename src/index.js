205ad489d6c838eba7f0a0a73a15696722cb6364
TamasParkany <thomasovmail@gmail.com>
2022-02-11 06:19:18 +0100

    updated hamburger and animations

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ function hideForm(e) {
   }
 }
 
-//hide navbar on scroll
-
+// NAVBAR HIDER
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   if (window.screen.width > 1180) {
@@ -35,7 +34,11 @@ window.onscroll = function () {
   }
 };
 
-// mobile menu
+// MOBILE MENU
+const mcMenu = document.getElementsByClassName("menu")[0];
+
+mcMenu.addEventListener("click", () => mcMenu.classList.toggle("open"));
+
 let transitionState = "start";
 
 document
@@ -92,3 +95,61 @@ function toggleMMenu() {
     transitionState = "start";
   }
 }
+
+//ANIMATION OBSERVER
+document.documentElement.style.setProperty("--animate-duration", "2s");
+
+const ambitionsCounterObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const projects = entry.target.querySelector(
+      ".projects .ambitions-counter-number"
+    );
+    const negotiations = entry.target.querySelector(
+      ".negotiations .ambitions-counter-number"
+    );
+    const deals = entry.target.querySelector(
+      ".deals .ambitions-counter-number"
+    );
+
+    //VALUE COUNTER
+    function animateValue(obj, start, end, duration) {
+      let startTimestamp = null;
+      const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+          window.requestAnimationFrame(step);
+        }
+      };
+      window.requestAnimationFrame(step);
+    }
+
+    if (entry.isIntersecting) {
+      animateValue(projects, 0, 19, 1500);
+      animateValue(negotiations, 0, 253, 1500);
+      animateValue(deals, 0, 60, 1500);
+      return;
+    }
+    // stop intersecting
+  });
+});
+
+ambitionsCounterObserver.observe(document.querySelector(".ambitions-counter"));
+
+const teamBottomObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const people = entry.target.getElementsByClassName("card");
+
+    if (entry.isIntersecting) {
+      people[0].classList.add("animate__fadeInUp");
+      people[1].classList.add("animate__fadeInDown");
+      people[2].classList.add("animate__fadeInUp");
+      people[3].classList.add("animate__fadeInDown");
+      return;
+    }
+    // stop intersecting
+  });
+});
+
+teamBottomObserver.observe(document.querySelector(".team-bottom"));
